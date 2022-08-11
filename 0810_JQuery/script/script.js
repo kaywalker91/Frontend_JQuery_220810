b4afdf3f7f606599950ba6d667dbf2db0349a8bd
Kim Dae Gak <104833740+kaywalker91@users.noreply.github.com>
2022-08-11 15:56:39 +0900

    Add files via upload

--- 0810_JQuery/script/script.js
+++ 0810_JQuery/script/script.js
@@ -15,19 +15,47 @@ $(document).ready(function(){
 
     });
 
-    // $("#img2").click(function(){
+    $("#img1").click(function(){
 
-    //     var juso = $("this").attr("src");
-    //     var title = $("this").attr("alt");
-    //     var data = $("this").attr("cont");
-
-    //     $("#logo").attr("src", "juso");
-    //     $("#gal h1").attr("title");
-
-    // });
-
-    $().click(function(){
-
+        $("#logo").attr("src", "images/image1.jpg");
+        $("#title").text("적막한 사막");
+        $("p").text("적막한 사막에는 바람이 불고 곧 태풍이 올 낌새가 보인다.");
+       
     });
+
+    $("#img2").click(function(){
+
+        $("#logo").attr("src", "images/image2.jpg");
+        $("#title").text("아름다운 꽃");
+        $("p").text("하얀 꽃이 이쁘다!");
+       
+    });
+
+    $("#img3").click(function(){
+
+        $("#logo").attr("src", "images/image3.jpg");
+        $("h1").text("신기한 해파리");
+        $("p").text("해파리가 바다 속을 헤엄친다.");
+       
+    });
+
+    $("#img4").click(function(){
+
+        $("#logo").attr("src", "images/image4.jpg");
+        $("h1").text("웃는 코알라");
+        $("p").text("귀여운 코알라가 웃고 있다.");
+       
+    });
+
+    $("#img5").click(function(){
+
+        $("#logo").attr("src", "images/image5.jpg");
+        $("h1").text("등 대");
+        $("p").text("저녁노을 빛이 비추는 등대의 모습.");
+       
+    });
+
+
+
 
 });
