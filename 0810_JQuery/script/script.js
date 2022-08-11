a664598d66f6bb493eb169f6b7e4b730648e7dc7
Kim Dae Gak <104833740+kaywalker91@users.noreply.github.com>
2022-08-11 14:33:44 +0900

    Add files via upload

---
+++ 0810_JQuery/script/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    // var txt = $("#para1").text();
+    // alert(txt);
+
+    // $("#para1").html("<b>Hello</b> World!");
+
+    $("#btn1").click(function(){
+        
+        // var txt = $("#input1").val();
+        // alert(txt);
+
+        var menu = $("#input1").val();
+        alert(menu);
+
+    });
+
+    // $("#img2").click(function(){
+
+    //     var juso = $("this").attr("src");
+    //     var title = $("this").attr("alt");
+    //     var data = $("this").attr("cont");
+
+    //     $("#logo").attr("src", "juso");
+    //     $("#gal h1").attr("title");
+
+    // });
+
+    $().click(function(){
+
+    });
+
+});
